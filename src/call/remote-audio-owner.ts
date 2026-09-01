932305be165ecbead1e533dbc0be6742980883ef
1sl2tp <1sl2tp@gmail.com>
2026-09-01 08:51:05 +0700

    feat(call): own remote audio element

---
+++ src/call/remote-audio-owner.ts
@@ -0,0 +1,17 @@
+export interface RemoteAudioOwnerDeps {
+  createAudio?: () => HTMLAudioElement
+  createStream?: (tracks: MediaStreamTrack[]) => MediaStream
+}
+
+export function createOwnedRemoteAudio(
+  track: MediaStreamTrack,
+  deps: RemoteAudioOwnerDeps = {},
+): HTMLAudioElement {
+  const element = deps.createAudio ? deps.createAudio() : document.createElement('audio')
+  const stream = deps.createStream ? deps.createStream([track]) : new MediaStream([track])
+
+  element.autoplay = true
+  element.playsInline = true
+  element.srcObject = stream
+  return element
+}
