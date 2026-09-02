d44dae6c61c7c20a4fbb88b358ad9a0ab25ac6c2
1sl2tp <1sl2tp@gmail.com>
2026-09-02 20:04:03 +0700

    feat: switch User account owner by presentation

--- src/user-shell.ts
+++ src/user-shell.ts
@@ -1,5 +1,12 @@
 import './user-main'
 import './user/account-ui.css'
+import './ui/chatwoot-port/account/account.css'
+import { mountUserChatwootAccountUi } from './user/chatwoot-account-ui'
 import { mountUserAccountUi } from './user/account-ui'
+import { getChatPresentation } from './ui/chatwoot-port/presentation-switch'
 
-mountUserAccountUi()
+if (getChatPresentation() === 'chatwoot-port') {
+  mountUserChatwootAccountUi()
+} else {
+  mountUserAccountUi()
+}
