993e9f0440081045eb163a4538de5dabf93caef6
1sl2tp <1sl2tp@gmail.com>
2026-09-03 05:38:14 +0700

    feat: show release on user web

--- src/user-shell.ts
+++ src/user-shell.ts
@@ -1,8 +1,10 @@
 import './user-main'
 import './user/account-ui.css'
 import './ui/chatwoot-port/account/account.css'
+import { mountReleaseBadge } from './release'
 import { mountUserChatwootAccountUi } from './user/chatwoot-account-ui'
 import { mountUserChatwootLoginUi } from './user/chatwoot-login-ui'
 
+mountReleaseBadge()
 mountUserChatwootLoginUi()
 mountUserChatwootAccountUi()
