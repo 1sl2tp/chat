abd1345e1d323ad106beef6c6cfa44c1149919b6
1sl2tp <1sl2tp@gmail.com>
2026-09-02 20:13:16 +0700

    feat: cut Hỗ trợ shell over to Chatwoot Inbox owner

--- src/admin-shell.ts
+++ src/admin-shell.ts
@@ -1,15 +1,6 @@
 import './admin-main'
 import './admin/management-ui.css'
-import './admin/zalo-polish.css'
 import './ui/chatwoot-port/inbox/inbox.css'
 import { mountAdminChatwootManagementUi } from './admin/chatwoot-management-ui'
-import { mountAdminManagementUi } from './admin/management-ui'
-import { mountAdminZaloPolish } from './admin/zalo-polish'
-import { getChatPresentation } from './ui/chatwoot-port/presentation-switch'
 
-if (getChatPresentation() === 'chatwoot-port') {
-  mountAdminChatwootManagementUi()
-} else {
-  mountAdminManagementUi()
-  mountAdminZaloPolish()
-}
+mountAdminChatwootManagementUi()
