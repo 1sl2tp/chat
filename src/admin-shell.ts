918faef6eea92c79a8f39a80ec653e5076001cf9
1sl2tp <1sl2tp@gmail.com>
2026-09-03 05:38:22 +0700

    feat: show release on admin web

--- src/admin-shell.ts
+++ src/admin-shell.ts
@@ -3,6 +3,8 @@ import './admin/management-ui.css'
 import './ui/chatwoot-port/inbox/inbox.css'
 import { mountAdminChatwootManagementUi } from './admin/chatwoot-management-ui'
 import { installAdminChatwootLoginUi } from './admin/chatwoot-login-ui'
+import { mountReleaseBadge } from './release'
 
+mountReleaseBadge()
 installAdminChatwootLoginUi()
 mountAdminChatwootManagementUi()
