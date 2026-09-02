fef32421a1985be0edd9040d746de8394542b9b4
1sl2tp <1sl2tp@gmail.com>
2026-09-02 16:30:14 +0700

    ui: mount Zalo-like admin polish

--- src/admin-shell.ts
+++ src/admin-shell.ts
@@ -1,5 +1,8 @@
 import './admin-main'
 import './admin/management-ui.css'
+import './admin/zalo-polish.css'
 import { mountAdminManagementUi } from './admin/management-ui'
+import { mountAdminZaloPolish } from './admin/zalo-polish'
 
 mountAdminManagementUi()
+mountAdminZaloPolish()
