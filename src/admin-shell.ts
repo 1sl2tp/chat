db8dc453b8b79781054124ff27e552e5ea0c00a5
1sl2tp <1sl2tp@gmail.com>
2026-09-02 20:35:25 +0700

    feat: mount Chatwoot LoginScreen in Hỗ trợ shell

--- src/admin-shell.ts
+++ src/admin-shell.ts
@@ -2,5 +2,7 @@ import './admin-main'
 import './admin/management-ui.css'
 import './ui/chatwoot-port/inbox/inbox.css'
 import { mountAdminChatwootManagementUi } from './admin/chatwoot-management-ui'
+import { installAdminChatwootLoginUi } from './admin/chatwoot-login-ui'
 
+installAdminChatwootLoginUi()
 mountAdminChatwootManagementUi()
