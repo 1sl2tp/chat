104786f3b403baafb69d691ac9b1152a486fde3b
1sl2tp <1sl2tp@gmail.com>
2026-09-02 20:12:56 +0700

    feat: cut User shell over to Chatwoot account owner

--- src/user-shell.ts
+++ src/user-shell.ts
@@ -2,11 +2,5 @@ import './user-main'
 import './user/account-ui.css'
 import './ui/chatwoot-port/account/account.css'
 import { mountUserChatwootAccountUi } from './user/chatwoot-account-ui'
-import { mountUserAccountUi } from './user/account-ui'
-import { getChatPresentation } from './ui/chatwoot-port/presentation-switch'
 
-if (getChatPresentation() === 'chatwoot-port') {
-  mountUserChatwootAccountUi()
-} else {
-  mountUserAccountUi()
-}
+mountUserChatwootAccountUi()
