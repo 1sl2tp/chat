08e9b83f800c9659886385748d2acceafd0b0e9c
1sl2tp <1sl2tp@gmail.com>
2026-09-04 06:25:58 +0700

    ui: remove unused Chatwoot sidebar sections

--- src/dashboard/components/layout/Sidebar.vue
+++ src/dashboard/components/layout/Sidebar.vue
@@ -8,34 +8,7 @@
 
     <div class="main-nav">
       <transition-group name="menu-list" tag="ul" class="menu vertical">
-        <sidebar-item
-          v-for="item in accessibleMenuItems"
-          :key="item.toState"
-          :menu-item="item"
-        />
-        <sidebar-item
-          v-if="shouldShowTeams"
-          :key="teamSection.toState"
-          :menu-item="teamSection"
-        />
-        <sidebar-item
-          v-if="shouldShowSidebarItem"
-          :key="inboxSection.toState"
-          :menu-item="inboxSection"
-        />
-        <sidebar-item
-          v-if="shouldShowSidebarItem"
-          :key="labelSection.toState"
-          :menu-item="labelSection"
-          @add-label="showAddLabelPopup"
-        />
-        <sidebar-item
-          v-if="showShowContactSideMenu"
-          :key="contactLabelSection.key"
-          :menu-item="contactLabelSection"
-          @add-label="showAddLabelPopup"
-        />
-      </transition-group>
+                                      </transition-group>
     </div>
 
     <div class="bottom-nav">
