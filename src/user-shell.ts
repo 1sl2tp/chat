0fa892dd7a745d43e6d4e79d7006b937fac8092b
1sl2tp <1sl2tp@gmail.com>
2026-09-03 07:10:59 +0700

    feat: cut User entrypoint to clean UI root

--- src/user-shell.ts
+++ src/user-shell.ts
@@ -1,11 +1,2 @@
-import './ui/reference.css'
-import './user-main'
-import './user/account-ui.css'
-import './ui/chatwoot-port/account/account.css'
-import { mountReleaseBadge } from './release'
-import { mountUserChatwootAccountUi } from './user/chatwoot-account-ui'
-import { mountUserChatwootLoginUi } from './user/chatwoot-login-ui'
-
-mountReleaseBadge()
-mountUserChatwootLoginUi()
-mountUserChatwootAccountUi()
+import './ui/clean/theme.css'
+import './user-clean-main'
