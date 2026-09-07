fcf964198cbef049ca8dbabb34f4259d267c625e
1sl2tp <1sl2tp@gmail.com>
2026-09-07 08:19:47 +0700

    release: bump audio-capture-policy.js to V21.72.27

--- audio-capture-policy.js
+++ audio-capture-policy.js
@@ -1,7 +1,7 @@
 (()=>{
 'use strict';
 
-const RELEASE_VERSION='V21.72.26';
+const RELEASE_VERSION='V21.72.27';
 const MODULE_CONTRACT_VERSION='audio-capture-v1';
 const DEVICE_KEY='taphoa.v21.audio.inputDeviceId';
 const BASE=Object.freeze({
