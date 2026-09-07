74c5f7d7e3d06955d7d87648daecc63611749373
1sl2tp <1sl2tp@gmail.com>
2026-09-07 08:57:30 +0700

    release: bump shell.js to V21.72.30

--- shell.js
+++ shell.js
@@ -1456,7 +1456,7 @@ AuthUI.renderAccountFooter();
 syncDesktopSidebarMode();
 
 window.ChatAppShell={
-  version:'V21.72.29',
+  version:'V21.72.30',
   NavigationCommand,
   CallCommand,
   AuthUI,
