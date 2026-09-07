ac4c54c6175c72c2d7e060976e4c0424b31eac5e
1sl2tp <1sl2tp@gmail.com>
2026-09-07 11:15:51 +0700

    V21.72.39 release marker sync

--- shell.js
+++ shell.js
@@ -1462,7 +1462,7 @@ AuthUI.renderAccountFooter();
 syncDesktopSidebarMode();
 
 window.ChatAppShell={
-  version:'V21.72.38',
+  version:'V21.72.39',
   NavigationCommand,
   CallCommand,
   AuthUI,
