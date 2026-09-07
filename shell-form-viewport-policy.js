677dabfc4c37a769652ee82cc78853023239724a
1sl2tp <1sl2tp@gmail.com>
2026-09-07 08:41:45 +0700

    release: bump shell-form-viewport-policy.js to V21.72.29

--- shell-form-viewport-policy.js
+++ shell-form-viewport-policy.js
@@ -1,7 +1,7 @@
 (()=>{
 'use strict';
 
-const RELEASE_VERSION='V21.72.28';
+const RELEASE_VERSION='V21.72.29';
 const MODULE_CONTRACT_VERSION='shell-form-viewport-v3';
 const KEYBOARD_THRESHOLD_PX=80;
 const EDGE_PX=12;
