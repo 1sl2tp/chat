8bce4c944c954126856d20596a53ac485e113627
1sl2tp <1sl2tp@gmail.com>
2026-09-19 06:42:22 +0700

    refactor: load composer actions from canonical build

--- quote-client.js
+++ quote-client.js
@@ -214,5 +214,3 @@ window.V21QuoteClient=Object.freeze({
   mount:mountQuotePanel,
 });
 })();
-
-void import('./admin-composer-actions.js').catch(error=>console.warn('[admin-composer-actions]',error));
