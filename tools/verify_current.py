73950f98567b7db5adb2a460ce6f3667815f17dd
1sl2tp <1sl2tp@gmail.com>
2026-09-20 11:01:56 +0700

    Verify Region 3 detail list surface

--- tools/verify_current.py
+++ tools/verify_current.py
@@ -72,6 +72,7 @@ subprocess.run([sys.executable,str(ROOT/'tests'/'test_v21_72_53_region3_work_row
 subprocess.run([sys.executable,str(ROOT/'tests'/'test_v21_72_54_region3_header_scroll_owner.py')],check=True,cwd=ROOT)
 subprocess.run([sys.executable,str(ROOT/'tests'/'test_v21_72_55_region3_detail_check_control.py')],check=True,cwd=ROOT)
 subprocess.run([sys.executable,str(ROOT/'tests'/'test_v21_72_56_region3_state_geometry.py')],check=True,cwd=ROOT)
+subprocess.run([sys.executable,str(ROOT/'tests'/'test_v21_72_57_region3_detail_list_surface.py')],check=True,cwd=ROOT)
 subprocess.run(['node',str(ROOT/'tests'/'test_v21_72_20_call_screen_wake_lock.js')],check=True,cwd=ROOT)
 subprocess.run(['node',str(ROOT/'tests'/'test_v21_zalo_webview_runtime_error.js')],check=True,cwd=ROOT)
 subprocess.run([sys.executable,'-m','pytest','-q',str(ROOT/'tests'/'test_v21_zalo_admin_ui_contract.py')],check=True,cwd=ROOT)
