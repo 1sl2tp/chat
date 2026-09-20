feba6cf6bd449a8649288154464d1e2a82b184df
1sl2tp <1sl2tp@gmail.com>
2026-09-20 10:12:18 +0700

    Verify Region 2 message action contract

--- tools/verify_current.py
+++ tools/verify_current.py
@@ -65,6 +65,7 @@ subprocess.run([sys.executable,str(ROOT/'tests'/'test_v21_72_46_top_mode_switch_
 subprocess.run([sys.executable,str(ROOT/'tests'/'test_v21_72_47_mobile_header_picker_keyboard_occlusion.py')],check=True,cwd=ROOT)
 subprocess.run([sys.executable,str(ROOT/'tests'/'test_v21_72_48_contact_tab_content_width.py')],check=True,cwd=ROOT)
 subprocess.run([sys.executable,str(ROOT/'tests'/'test_v21_72_49_region2_composer_menu_contract.py')],check=True,cwd=ROOT)
+subprocess.run([sys.executable,str(ROOT/'tests'/'test_v21_72_50_region2_message_action_contract.py')],check=True,cwd=ROOT)
 subprocess.run(['node',str(ROOT/'tests'/'test_v21_72_20_call_screen_wake_lock.js')],check=True,cwd=ROOT)
 subprocess.run(['node',str(ROOT/'tests'/'test_v21_zalo_webview_runtime_error.js')],check=True,cwd=ROOT)
 subprocess.run([sys.executable,'-m','pytest','-q',str(ROOT/'tests'/'test_v21_zalo_admin_ui_contract.py')],check=True,cwd=ROOT)
