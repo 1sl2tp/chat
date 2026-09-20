166bc921a652263938f176d5097d4caff481300d
1sl2tp <1sl2tp@gmail.com>
2026-09-20 12:03:14 +0700

    Verify global mobile dynamic viewport owner

--- tools/verify_current.py
+++ tools/verify_current.py
@@ -81,6 +81,7 @@ subprocess.run([sys.executable,str(ROOT/'tests'/'test_v21_72_62_profile_delete_c
 subprocess.run([sys.executable,str(ROOT/'tests'/'test_v21_72_63_settings_modal_layer_contract.py')],check=True,cwd=ROOT)
 subprocess.run([sys.executable,str(ROOT/'tests'/'test_v21_72_64_settings_modal_dynamic_viewport.py')],check=True,cwd=ROOT)
 subprocess.run([sys.executable,str(ROOT/'tests'/'test_v21_72_65_profile_modal_dynamic_viewport.py')],check=True,cwd=ROOT)
+subprocess.run([sys.executable,str(ROOT/'tests'/'test_v21_72_66_global_mobile_dynamic_viewport.py')],check=True,cwd=ROOT)
 subprocess.run(['node',str(ROOT/'tests'/'test_v21_72_20_call_screen_wake_lock.js')],check=True,cwd=ROOT)
 subprocess.run(['node',str(ROOT/'tests'/'test_v21_zalo_webview_runtime_error.js')],check=True,cwd=ROOT)
 subprocess.run([sys.executable,'-m','pytest','-q',str(ROOT/'tests'/'test_v21_zalo_admin_ui_contract.py')],check=True,cwd=ROOT)
